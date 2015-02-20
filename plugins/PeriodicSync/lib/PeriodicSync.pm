bf4af1acc13e47de9e034fb7f1f7b67d6ac47de3
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2015-02-20 07:47:35 +0000

    Version 0.01.

---
+++ plugins/PeriodicSync/lib/PeriodicSync.pm
@@ -0,0 +1,68 @@
+package PeriodicSync;
+use strict;
+use warnings;
+use utf8;
+
+use MT::Util qw( ts2epoch epoch2ts );
+use MT::TheSchwartz::Job;
+
+sub override {
+    if ( !MT->component('Sync') ) {
+        return;
+    }
+
+    require MT::Worker::ContentsSync;
+    my $work = \&MT::Worker::ContentsSync::work;
+
+    no warnings 'redefine';
+    *MT::Worker::ContentsSync::work = \&_work;
+}
+
+sub _work {
+    my ( $class, $job ) = @_;
+    $work->( $class, $job );
+
+    # Do nothing when an error occurs.
+    if (MT::TheSchwartz::Job->exist(
+            { funcid => $job->funcid, uniqkey => $job->uniqkey }
+        )
+        )
+    {
+        return;
+    }
+
+    my $plugin = MT->component('PeriodicSync');
+    my $scope  = 'blog:' . $job->uniqkey;
+
+    my $sync_period_status
+        = $plugin->get_config_value( 'sync_period_status', $scope );
+    my $sync_period = $plugin->get_config_value( 'sync_period', $scope );
+
+    # Do nothing when sync_period is invalid.
+    unless ( $sync_period_status
+        && $sync_period
+        && $sync_period =~ m/^\d+$/ )
+    {
+        return;
+    }
+
+    require MT::SyncSetting;
+    my $sync_setting = MT::SyncSetting->load( { blog_id => $job->uniqkey } )
+        or return;
+
+    my $epoch = ts2epoch( undef, $sync_setting->schedule_date );
+    $epoch += $sync_period * 60 * 60;
+    my $ts = epoch2ts( undef, $epoch );
+
+    # Update mt_sync_setting record.
+    $sync_setting->schedule_date($ts);
+    $sync_setting->save or die $sync_setting->errstr;
+
+    # Create mt_ts_job record.
+    $job->run_after($epoch);
+    $job->save or die $job->errstr;
+
+    return;
+}
+
+1;
---
+++ plugins/PeriodicSync/lib/PeriodicSync/CMS.pm
@@ -0,0 +1,75 @@
+package PeriodicSync::CMS;
+use strict;
+use warnings;
+use utf8;
+
+# Add form elements to Contents Sync Settings screen.
+sub add_form {
+    my ( $cb, $app, $tmpl ) = @_;
+
+    my $insert = quotemeta <<'__INSERT__';
+      <div id="sync_date-field-msg-block" style="display: none;"></div>
+__INSERT__
+    my $mtml = <<'__MTML__';
+      <div class="option"><input type="checkbox" id="sync-period-status" name="sync_period_status" value="1" <mt:if name="sync_period_status">checked="checked"</mt:if> /> <input type="text" id="sync-period" class="text num" name="sync_period" value="<mt:var name="sync_period">" /> 時間毎に実行する。</div>
+
+<mt:setvarblock name="jq_js_include" append="1">
+var changeSyncPeriodStatus = function() {
+  if (jQuery('input#sync-period-status').is(':checked')) {
+    jQuery('input#sync-period').removeAttr('disabled');
+  } else {
+    jQuery('input#sync-period').attr('disabled','disabled');
+  }
+};
+jQuery(document).ready(changeSyncPeriodStatus);
+jQuery('input#sync-period-status').change(changeSyncPeriodStatus);
+</mt:setvarblock>
+__MTML__
+    $$tmpl =~ s/($insert)/$mtml$1/;
+}
+
+# Load parameters "sync_period_status" and "sync_period".
+sub load_param {
+    my ( $cb, $app, $param, $tmpl ) = @_;
+
+    my $plugin = MT->component('PeriodicSync');
+    my $scope  = 'blog:' . $app->blog->id;
+
+    $param->{sync_period_status}
+        = $plugin->get_config_value( 'sync_period_status', $scope );
+    $param->{sync_period}
+        = $plugin->get_config_value( 'sync_period', $scope );
+
+    return 1;
+}
+
+# Save parameters "sync_period_status" and "sync_period".
+# An error occurs when "sync_period" is not positive integer.
+sub save_param {
+    my ( $cb, $app, $sync_setting, $original ) = @_;
+
+    my $sync_period_status = $app->param('sync_period_status') ? 1 : undef;
+    my $sync_period = $app->param('sync_period');
+
+    if ($sync_period_status) {
+        unless ( $sync_period && $sync_period =~ m/^\d$/ ) {
+            return $app->error(
+                'サーバー配信日時の配信間隔が不正です。1 以上の整数値を設定してください。'
+            );
+        }
+    }
+
+    my $plugin = MT->component('PeriodicSync');
+    my $scope  = 'blog:' . $app->blog->id;
+
+    $plugin->set_config_value( 'sync_period_status', $sync_period_status,
+        $scope );
+    if ($sync_period_status) {
+        $plugin->set_config_value( 'sync_period', $sync_period, $scope );
+    }
+
+    return 1;
+}
+
+1;
+
